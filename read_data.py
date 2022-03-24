90aad90eb98b5a61f83009c0bfd088e6c9b50e38
UUtung <32029557+wuyentung@users.noreply.github.com>
2022-03-24 21:52:09 +0800

    生 df

--- read_data.py
+++ read_data.py
@@ -5,13 +5,12 @@ import numpy as np
 #%%
 files = os.listdir("./data")
 #%%
-life2018_pd = pd.read_excel("./data/%s" %files[0], header=3, index_col=0)
-#%%
-life2018_pd.shape
-#%%
-life2018_pd.columns
+life2018_raw_df = pd.read_excel("./data/%s" %files[0], header=3, index_col=0)
+life2019_raw_df = pd.read_excel("./data/%s" %files[1], header=3, index_col=0)
+life2020_raw_df = pd.read_excel("./data/%s" %files[2], header=3, index_col=0)
 #%%
 ENG_NAMES = ['Bank Taiwan Life', 'Taiwan Life', 'PCA Life', 'Cathay Life', 'China Life', 'Nan Shan Life', 'Shin Kong Life', 'Fubon Life',  'Mercuries Life', 'Farglory Life', 'Hontai Life', 'Allianz Taiwan Life', 'Chunghwa Post', 'First-Aviva Life', 'BNP Paribas Cardif TCB', 'Prudential of Taiwan', 'CIGNA', 'Yuanta Life', 'TransGlobe Life', 'AIA Taiwan', 'Cardif', 'Chubb Tempest Life']
+CHI_NAMES = ['臺銀人壽', '台灣人壽', '保誠人壽', '國泰人壽', '中國人壽', '南山人壽', '新光人壽', '富邦人壽', '三商美邦人壽' '遠雄人壽', '宏泰人壽', '安聯人壽', '中華郵政', '第一金人壽', '合作金庫人壽', '保德信國際人壽', '康健人壽', '元大人壽', '全球人壽', '友邦人壽', '法國巴黎人壽', '安達人壽',]
 IDX = [
     '營業收入', 
     '\u3000簽單保費收入', 
@@ -69,8 +68,25 @@ IDX = [
     '本期綜合損益總額',
     ]
 #%%
-for idx in life2018_pd.index.tolist():
-    print(idx if isinstance(idx, str) and "收入" in idx else "")
+cal_insurance_exp = lambda df, col: df[col]['  承保費用支出'] + df[col]['  佣金費用']
+cal_operation_exp = lambda df, col: df[col]['  其他營業成本'] + df[col]['  財務成本'] + df[col]['營業費用']
+
+cal_insurance_income = lambda df, col: df[col]['\u3000簽單保費收入'] - df[col]['\u3000\u3000\u3000未滿期保費準備淨變動'] + df[col]['\u3000手續費收入']
+cal_reinsurance_exp = lambda df, col: df[col]['\u3000減：再保費支出']
+cal_reinsurance_income = lambda df, col: df[col]['\u3000再保費收入']
+
+cal_underwriting_profit = lambda df, col: df[col]['營業收入'] - df[col]['營業成本'] - df[col]['營業費用'] - df[col]['\u3000淨投資損益']
+cal_investment_profit = lambda df, col: df[col]['\u3000淨投資損益']
 #%%
-print([idx if isinstance(idx, str) else "" for idx in life2018_pd.index.tolist()])
+def single_insurer(df, name):
+    try:
+        df[name]
+    except:
+        if "First" in name:
+            name = "First Life"
+    return [cal_insurance_exp(df=df, col=name), cal_operation_exp(df=df, col=name), cal_insurance_income(df=df, col=name), cal_reinsurance_exp(df=df, col=name), cal_reinsurance_income(df=df, col=name), cal_underwriting_profit(df=df, col=name), cal_investment_profit(df=df, col=name), ]
+#%%
+LIFE2018 = pd.DataFrame([single_insurer(df=life2018_raw_df, name=name) for name in ENG_NAMES], index=ENG_NAMES, columns=["insurance_exp", "operation_exp", "insurance_income", "reinsurance_exp", "reinsurance_income", "underwriting_profit", "investment_profit"])
+LIFE2019 = pd.DataFrame([single_insurer(df=life2019_raw_df, name=name) for name in ENG_NAMES], index=ENG_NAMES, columns=["insurance_exp", "operation_exp", "insurance_income", "reinsurance_exp", "reinsurance_income", "underwriting_profit", "investment_profit"])
+LIFE2020 = pd.DataFrame([single_insurer(df=life2020_raw_df, name=name) for name in ENG_NAMES], index=ENG_NAMES, columns=["insurance_exp", "operation_exp", "insurance_income", "reinsurance_exp", "reinsurance_income", "underwriting_profit", "investment_profit"])
 #%%
