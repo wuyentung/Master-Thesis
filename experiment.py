175e54d8b740b5a510a9d71fada582d834bb87f7
UUtung <32029557+wuyentung@users.noreply.github.com>
2022-03-25 17:23:46 +0800

    個別能算，綜合不能算

--- experiment.py
+++ experiment.py
@@ -12,18 +12,59 @@ import pandas as pd
 import numpy as np
 import solver
 import solver_r
-from load_data import LIFE, LIFE2019, denoise_nonpositive, ATTRIBUTES
+from load_data import LIFE, LIFE2019, denoise_nonpositive, ATTRIBUTES, LIFE2018, LIFE2020
 #%%
-life2019_transformed = denoise_nonpositive(LIFE2019)
-eff_dict, lambdas_dict = solver.dea_dual(dmu=life2019_transformed.index, x=np.array(life2019_transformed[['insurance_exp', 'operation_exp']].T), y=np.array(life2019_transformed[['underwriting_profit', 'investment_profit']].T))
+#### 測試 #####
+life_transformed = denoise_nonpositive(LIFE)
+eff_dict, lambdas_dict = solver.dea_dual(dmu=life_transformed.index, x=np.array(life_transformed[['insurance_exp', 'operation_exp']].T), y=np.array(life_transformed[['underwriting_profit', 'investment_profit']].T))
 #%%
 eff_dict
 #%%
-px_19, py_19, lambdas_19 = solver.project_frontier(x=np.array(life2019_transformed[['insurance_exp', 'operation_exp']].T), y=np.array(life2019_transformed[['underwriting_profit', 'investment_profit']].T), rs="vrs", orient="IO")
+eff_dmu_name = []
+for key, value in eff_dict.items():
+    if round(value, 5) == 1:
+        eff_dmu_name.append(key)
+eff_dmu_name
 #%%
-peff_dict, plambdas_dict = solver.dea_dual(dmu=life2019_transformed.index, x=px_19, y=py_19)
+# life_transformed.T[eff_dmu_name].T
+eff_dict2, lambdas_dict2 = solver.dea_dual(dmu=eff_dmu_name, x=np.array(life_transformed.T[eff_dmu_name].T[['insurance_exp', 'operation_exp']].T), y=np.array(life_transformed.T[eff_dmu_name].T[['underwriting_profit', 'investment_profit']].T))
+#%%
+df = life_transformed.T[eff_dmu_name].T
+exp = dmp.get_smrts_dfs(dmu=[i for i in range(df.shape[0])], x=np.array(df[['insurance_exp', 'operation_exp']].T), y=np.array(df[['underwriting_profit', 'investment_profit']].T), trace=False, round_to=5, dmu_wanted=None)
+#%%
+px_19, py_19, lambdas_19 = solver.project_frontier(x=np.array(life_transformed[['insurance_exp', 'operation_exp']].T), y=np.array(life_transformed[['underwriting_profit', 'investment_profit']].T), rs="vrs", orient="IO")
+#%%
+peff_dict, plambdas_dict = solver.dea_dual(dmu=life_transformed.index, x=px_19, y=py_19)
 #%%
 peff_dict
+#### 測試結束 #####
 #%%
-exp = dmp.get_smrts_dfs(dmu=[i for i in range(px_19.shape[1])], x=px_19, y=py_19, trace=False, round_to=5, dmu_wanted=None)
+def sys_smrts(df:pd.DataFrame, project=False):
+    ## transform data
+    ## s-MRTS for  whole system
+    transformed_df = denoise_nonpositive(df)
+            
+    ## project all dmu to VRS frontier in IO
+    if project:
+        px, py, lambdas = solver.project_frontier(x=np.array(transformed_df[['insurance_exp', 'operation_exp']].T), y=np.array(transformed_df[['underwriting_profit', 'investment_profit']].T), rs="vrs", orient="IO")
+        exp = dmp.get_smrts_dfs(dmu=[i for i in range(px.shape[1])], x=px, y=py, trace=False, round_to=5, dmu_wanted=None)
+        return exp
+    
+    eff_dict, lambdas_dict = solver.dea_dual(dmu=transformed_df.index, x=np.array(transformed_df[['insurance_exp', 'operation_exp']].T), y=np.array(transformed_df[['underwriting_profit', 'investment_profit']].T))
+
+    eff_dmu_name = []
+    for key, value in eff_dict.items():
+        if round(value, 5) == 1:
+            eff_dmu_name.append(key)
+    
+    df = transformed_df.T[eff_dmu_name].T
+    exp = dmp.get_smrts_dfs(dmu=[i for i in range(df.shape[0])], x=np.array(df[['insurance_exp', 'operation_exp']].T), y=np.array(df[['underwriting_profit', 'investment_profit']].T), trace=False, round_to=5, dmu_wanted=None)
+    return exp
 #%%
+expALL = sys_smrts(df=LIFE)
+#%%
+exp18 = sys_smrts(df=LIFE2018)
+exp19 = sys_smrts(df=LIFE2019)
+exp20 = sys_smrts(df=LIFE2020)
+#%%
+## 好奇怪，個別年的算得出來，綜合在一起卻算不出來，不知道會不會是 transform 的問題
