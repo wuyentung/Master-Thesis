cf3e4776a0d3d2f85326cbf5fdb1884db5079ef2
wuyentung <wuyentung@gmail.com>
2022-05-12 17:00:49 +0800

    refactor(solver.py): moving project data to returns of dea_dual()

--- solver.py
+++ solver.py
@@ -25,16 +25,20 @@ def dea_dual(dmu:list, x:np.ndarray, y:np.ndarray,  THRESHOLD=0.000000000001, or
     ## vrs dual dea solver using solver_r.io_vrs_dual() or solver_r.oo_vrs_dual()
     eff_dict = {}
     lambdas_dict = {}
+    projected_x = x.copy()
+    projected_y = y.copy()
     K = len(dmu)
     if "IO" == orient:
         for r in range(K):
             eff_dict[dmu[r]], lambdas_dict[dmu[r]] = solver_r.io_dual(dmu=dmu, r=r, x=x, y=y, THRESHOLD=THRESHOLD, rs=rs)
+            projected_x[:, r] *= eff_dict[dmu[r]]
     elif "OO" == orient:
         for r in range(K):
             eff_dict[dmu[r]], lambdas_dict[dmu[r]] = solver_r.oo_dual(dmu=dmu, r=r, x=x, y=y, THRESHOLD=THRESHOLD, rs=rs)
+            projected_y[:, r] *= eff_dict[dmu[r]]
     else:
         raise ValueError("only 'IO' or 'OO' can ba calculated") 
-    return eff_dict, lambdas_dict
+    return eff_dict, lambdas_dict, projected_x, projected_y
 #%%
 def dea_vrs(dmu:list, x:np.ndarray, y:np.ndarray,  THRESHOLD=0.000000000001, orient="OO"):
     ## vrs dual dea solver using solver_r.io_vrs_dual() or solver_r.oo_vrs_dual()
@@ -53,20 +57,20 @@ def dea_vrs(dmu:list, x:np.ndarray, y:np.ndarray,  THRESHOLD=0.000000000001, ori
         raise ValueError("only 'IO' or 'OO' can ba calculated") 
     return eff_dict, v_dict, u_dict, intercept_dict
 #%%
-def project_frontier(x:np.ndarray, y:np.ndarray, orient="IO", rs="vrs"):
-    projected_x = x.copy()
-    projected_y = y.copy()
-    lamdas = {}
-    K = x.shape[1]
-    for r in range(K):
-        if "IO" == orient:
-            obj, vars_dict = solver_r.io_dual(dmu=[i for i in range(K)], r=r, x=x, y=y, rs=rs)
-            projected_x[:, r] *= obj
-        elif "OO" == orient:
-            obj, vars_dict = solver_r.oo_dual(dmu=[i for i in range(K)], r=r, x=x, y=y, rs=rs)
-            projected_y[:, r] *= obj
-        else:
-            raise ValueError("only 'IO' or 'OO' can ba calculated") 
-        lamdas[r] = vars_dict
+# def project_frontier(x:np.ndarray, y:np.ndarray, orient="IO", rs="vrs"):
+#     projected_x = x.copy()
+#     projected_y = y.copy()
+#     lamdas = {}
+#     K = x.shape[1]
+#     for r in range(K):
+#         if "IO" == orient:
+#             obj, vars_dict = solver_r.io_dual(dmu=[i for i in range(K)], r=r, x=x, y=y, rs=rs)
+#             projected_x[:, r] *= obj
+#         elif "OO" == orient:
+#             obj, vars_dict = solver_r.oo_dual(dmu=[i for i in range(K)], r=r, x=x, y=y, rs=rs)
+#             projected_y[:, r] *= obj
+#         else:
+#             raise ValueError("only 'IO' or 'OO' can ba calculated") 
+#         lamdas[r] = vars_dict
         
-    return projected_x, projected_y, lamdas
+#     return projected_x, projected_y, lamdas
